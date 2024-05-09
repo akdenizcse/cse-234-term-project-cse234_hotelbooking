de609966bfecce1312370f18007f5800ac90c5a7
emres <emresmz13@gmail.com>
2024-05-09 22:58:56 +0300

    Login Page added

---
+++ app/src/main/java/com/example/hotelbooking/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.example.hotelbooking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.hotelbooking.databinding.ActivityLoginBinding
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    lateinit var username : EditText
+    lateinit var password: EditText
+    lateinit var loginButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener(View.OnClickListener {
+            if (binding.username.text.toString() == "user" && binding.password.text.toString() == "1234"){
+                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
