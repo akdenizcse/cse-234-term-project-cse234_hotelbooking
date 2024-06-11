830a915fdfa2ad726e01e22ab6ca8a53c5d80fd7
Piko59 <emresmz13@gmail.com>
2024-06-11 03:13:53 +0300

    Changes at admin page

--- app/src/main/java/com/example/hotelbooking/AdminPageActivity.kt
+++ app/src/main/java/com/example/hotelbooking/AdminPageActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hotelbooking.databinding.ActivityAdminPageBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class AdminPageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAdminPageBinding
@@ -21,6 +22,17 @@ class AdminPageActivity : AppCompatActivity() {
         binding.addRoomButton.setOnClickListener {
             val intent = Intent(this, AddRoomActivity::class.java)
             startActivity(intent)
+        }
+
+        // Logout button
+        binding.logoutButton.setOnClickListener {
+            // Çıkış yap
+            FirebaseAuth.getInstance().signOut()
+
+            // LoginActivity'e geri dön
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
